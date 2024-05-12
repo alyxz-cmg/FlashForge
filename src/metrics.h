6b8c1e472ea4a050b25c4973f8cf7143ea872569
alyxz-cmg <callmegreninja@gmail.com>
2024-05-11 17:49:51 -0700

    added read and trim operations with mapping validation

--- src/metrics.h
+++ src/metrics.h
@@ -10,13 +10,19 @@ namespace FlashForge {
 class MetricsCollector {
 public:
     uint64_t host_writes = 0;
+    uint64_t host_reads = 0;
+    uint64_t host_trims = 0;
     uint64_t flash_writes = 0;
+    uint64_t flash_reads = 0;
     uint64_t erase_ops = 0;
     uint64_t gc_events = 0;
     uint64_t gc_pages_moved = 0;
 
     void record_host_write() { host_writes++; }
+    void record_host_read() { host_reads++; }
+    void record_host_trim() { host_trims++; }
     void record_flash_write() { flash_writes++; }
+    void record_flash_read() { flash_reads++; }
     void record_erase() { erase_ops++; }
     void record_gc(uint32_t pages_moved) {
         gc_events++;
